aa717986a22edbaadef569c5d00af2e8c68d370b
RANGESHPANDIAN PT <rangeshpandian@gmail.com>
2025-12-02 09:51:07 +0530

    Refactor List.py to include item access examples

--- List.py
+++ List.py
@@ -55,3 +55,24 @@ print(type(mylist))
 
 thislist = list(("apple", "banana", "cherry")) # note the double round-brackets
 print(thislist)
+
+#Python - Access List Items
+thislist = ["apple", "banana", "cherry"]
+print(thislist[1])
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist[-1])
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5])
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[:4])
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:])
+
+#Check if Item Exists
+thislist = ["apple", "banana", "cherry"]
+if "apple" in thislist:
+    print("Yes, 'apple' is in the fruits list")
