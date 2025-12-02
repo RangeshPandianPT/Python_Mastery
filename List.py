78396b21e95e183591eae2d58ae60fb19f480ddd
RANGESHPANDIAN PT <rangeshpandian@gmail.com>
2025-12-02 11:21:55 +0530

    Update List.py

--- List.py
+++ List.py
@@ -76,3 +76,42 @@ print(thislist[2:])
 thislist = ["apple", "banana", "cherry"]
 if "apple" in thislist:
     print("Yes, 'apple' is in the fruits list")
+
+#Python - Change List Items
+thislist = ["apple", "banana", "cherry"]
+thislist[1] = "blackcurrant"
+print(thislist)
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+thislist[1:3] = ["blackcurrant", "watermelon"]
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thislist[1:2] = ["blackcurrant", "watermelon"]
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thislist[1:3] = ["watermelon"]
+print(thislist)
+
+#Insert Items
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(2, "watermelon")
+print(thislist)
+
+#Python - Add List Items
+thislist = ["apple", "banana", "cherry"]
+thislist.append("orange")
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(1, "orange")
+print(thislist)
+
+#Extend List
+thislist = ["apple", "banana", "cherry"]
+tropical = ["mango", "pineapple", "papaya"]
+thislist.extend(tropical)
+print(thislist)
+
+#Add Any Iterable
