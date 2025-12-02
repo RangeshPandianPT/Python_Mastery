d1131c508eb39ac653fb5084b730bff76daec036
RANGESHPANDIAN PT <rangeshpandian@gmail.com>
2025-12-02 09:39:28 +0530

    Add files via upload

---
+++ List.py
@@ -0,0 +1,57 @@
+#Python Lists
+mylist = ["apple", "banana", "cherry"]
+
+"""
+List
+Lists are used to store multiple items in a single variable.
+
+Lists are one of 4 built-in data types in Python used to store collections of data, the other 3 are Tuple, Set, and Dictionary, all with different qualities and usage.
+
+Lists are created using square brackets:
+"""
+
+#ExampleGet your own Python Server
+#Create a List:
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+
+#Allow Duplicates
+#Since lists are indexed, lists can have items with the same value:
+
+#Example
+#Lists allow duplicate values:
+
+thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+print(thislist)
+
+#List Length
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist))
+
+#List Items - Data Types
+#Example
+#String, int and boolean data types:
+
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+
+#Example
+#A list with strings, integers and boolean values:
+
+list1 = ["abc", 34, True, 40, "male"]
+
+#type()
+#Example
+#What is the data type of a list?
+
+mylist = ["apple", "banana", "cherry"]
+print(type(mylist))
+
+#The list() Constructor
+#Example
+#Using the list() constructor to make a List:
+
+thislist = list(("apple", "banana", "cherry")) # note the double round-brackets
+print(thislist)
